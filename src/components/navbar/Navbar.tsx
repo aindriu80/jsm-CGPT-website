c3af442d2e7f2e4408dec1b771bb57528720f235
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-12-19 16:55:45 +0000

    Starting Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './navbar.css'
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+
+// BEM -> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar </div>
-  )
-}
+    <div className="gpt__navbar">
+      {" "}
+      <div className="gpt__navbar-links">
+        {" "}
+        <div className="gpt__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar 
+export default Navbar;
