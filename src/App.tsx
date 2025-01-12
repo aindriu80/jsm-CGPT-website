1c8cd41f310e20d87e778973cd062e8a24433ae6
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-01-12 17:00:38 +0000

    Adding the brands

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import Footer from './containers/footer/Footer'
-import Blog from './containers/blog/Blog'
-import Possibility from './containers/possibility/Possibility'
-import Features from './containers/features/Features'
-import WhatGPT from './containers/whatGPT5/WhatGPT5'
-import Header from './containers/header/Header'
-import CTA from './components/cta/CTA'
-import Brand from './components/brand/Brand'
-import Navbar from './components/navbar/Navbar'
-import './App.css'
-
+import Footer from "./containers/footer/Footer";
+import Blog from "./containers/blog/Blog";
+import Possibility from "./containers/possibility/Possibility";
+import Features from "./containers/features/Features";
+import WhatGPT from "./containers/whatGPT5/WhatGPT5";
+import Header from "./containers/header/Header";
+import CTA from "./components/cta/CTA";
+import Brand from "./components/brand/Brand";
+import Navbar from "./components/navbar/Navbar";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <div className="gradient__bg">
         <Navbar />
         <Header />
@@ -25,7 +24,7 @@ const App = () => {
       <Blog />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
