3e0179269ca20194b42777c40e7c77adffdba382
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-01-13 17:02:33 +0000

    WhatGPT section

--- src/containers/whatGPT5/WhatGPT5.tsx
+++ src/containers/whatGPT5/WhatGPT5.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './whatGPT.css'
+import Feature from "../../components/feature/Feature";
+import "./whatGPT.css";
 
 const WhatGPT = () => {
   return (
-    <div>WhatGPT </div>
-  )
-}
+    <>
+      <div className="gpt__whatgpt section__margin" id="whpt">
+        <div className="gpt__whatgpt-feature">
+          <Feature />
+        </div>
+        <div className="gpt__whatgpt-heading">
+          <h1 className="gradient__text">
+            The possibilities are beyond your imagination
+          </h1>
+          <p>Explore The Library</p>
+        </div>
+        <div className="gpt__whatgpt-container">
+          <Feature />
+          <Feature />
+          <Feature />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default WhatGPT 
+export default WhatGPT;
