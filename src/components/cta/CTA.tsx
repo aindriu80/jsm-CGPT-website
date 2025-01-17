77de1a1d81accdf9fef8a5a5f27ad09ed37f2bf7
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-01-17 17:12:08 +0000

    CTA section

--- src/components/cta/CTA.tsx
+++ src/components/cta/CTA.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './cta.css'
+import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA </div>
-  )
-}
+    <div className="gpt__cta">
+      <div className="gpt__cta-content">
+        <p>Request Early Access</p>
+        <h3>Register today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CTA 
+export default CTA;
